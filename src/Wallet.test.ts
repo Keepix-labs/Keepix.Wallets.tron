950089084aa69e4ef0755c362cdf3fa579d51650
jode-vri <jode-vri@student.42.fr>
2023-12-11 21:07:34 +0100

    Update dependencies and add wallet functionality

--- src/Wallet.test.ts
+++ src/Wallet.test.ts
@@ -35,5 +35,65 @@ const mockFetch = () => {
 }
 
 describe('basic wallet', () => {
-    
+  const mnemonic = 'cannon cage satoshi scrub interest border disorder silver access cement bicycle supply allow cigar blast shove earn poverty aware frog page evolve spoil cattle'
+  const privateKey = '216406fde0e75a338fde460144a457ece06a51c5de38455520412e99ee9c7491';
+  const address = 'TDBmkXzgbYyjKrcY4c6mDM78i3YiTkjLLZ';
+
+  it('can generate with random', async () => {
+    const wallet = new Wallet({});
+
+    expect(wallet.getAddress()).toBeDefined();
+    expect(wallet.getPrivateKey).toBeDefined();
+    expect(wallet.getMnemonic()).toBeDefined();
+  }, 60000);
+
+  it('can generate same wallet', async () => {
+      const wallet = new Wallet({ password: 'toor' });
+
+      expect(wallet.getAddress()).toEqual(address);
+      expect(wallet.getPrivateKey()).toEqual(privateKey);
+      expect(wallet.getMnemonic()).toEqual(mnemonic);
+  }, 60000);
+
+  it('can generate with PrivateKey', async () => {
+      const wallet = new Wallet({ privateKey: privateKey,  });
+
+      expect(wallet.getAddress()).toEqual(address);
+      expect(wallet.getPrivateKey()).toEqual(privateKey);
+  }, 60000);
+
+  it('can generate with Mnemonic', async () => {
+      const wallet = new Wallet({ mnemonic: mnemonic });
+
+      expect(wallet.getAddress()).toEqual(address);
+      expect(wallet.getPrivateKey()).toEqual(privateKey);
+      expect(wallet.getMnemonic()).toEqual(mnemonic);
+  }, 60000);
+
+  it('can get balance', async () => {
+    const wallet = new Wallet({ password: 'toor' });
+
+    expect(await wallet.getCoinBalance()).toEqual('5.70456');
+  }, 60000);
+
+  it('can get token balance', async () => {
+    const wallet = new Wallet({ password: 'toor' });
+
+    expect(await wallet.getTokenBalance('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t', address)).toEqual('0.00000000');
+  }, 60000);
+
+  it('can estimate sendCoin', async () => {
+    const wallet = new Wallet({ password: 'toor' });
+    const estimationResult = await wallet.estimateCostSendCoinTo('Cd5PHnve4rJDHBAGvXgJnouddALyX3NmbwN3s3MzvPBQ', 1);
+
+    expect(estimationResult?.success).toBe(true);
+  });
+
+  it('can estimate sendToken', async () => {
+    const wallet = new Wallet({ password: 'toor' });
+    const estimationResult = await wallet.estimateCostSendTokenTo('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t', 'Cd5PHnve4rJDHBAGvXgJnouddALyX3NmbwN3s3MzvPBQ', 1);
+
+    expect(estimationResult?.success).toBe(false);
+    expect(estimationResult?.description).toMatch('insufficient funds');
+  });
 })
--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,4 +1,8 @@
+import TronWeb from 'tronweb';
 import { randomBytes } from 'crypto';
+import { entropyToMnemonic, mnemonicToEntropy, mnemonicToSeedSync } from 'bip39';
+
+const HttpProvider = TronWeb.providers.HttpProvider;
 
 function createPrivateKey(templatePrivateKey: string, password: string) {
     const crypto = require('crypto');
@@ -8,10 +12,13 @@ function createPrivateKey(templatePrivateKey: string, password: string) {
 
 export class Wallet {
 
+    private node: { fullNode: TronWeb.providers.HttpProvider, solidityNode: TronWeb.providers.HttpProvider; };
+    private tronWeb: TronWeb;
     private mnemonic?: string;
     private type: string = 'tron';
     private keepixTokens?: { coins: any, tokens: any };
     private rpc: any;
+    private wallet: { mnemonic: string, privateKey: string, publicKey: string };
 
     constructor({
         password,
@@ -19,7 +26,7 @@ export class Wallet {
         privateKey,
         keepixTokens,
         rpc,
-        privateKeyTemplate = '0x2050939757b6d498bb0407e001f0cb6db05c991b3c6f7d8e362f9d27c70128b9'
+        privateKeyTemplate = '81FB6642853A08E97622A759DB4277DFB9F8C23610908A4C3E7907F2BFD41EC7'
     }:{
         password?: string,
         mnemonic?: string,
@@ -28,61 +35,227 @@ export class Wallet {
         rpc?: any,
         privateKeyTemplate?: string,
     }) {
+        this.node = {
+            fullNode: new HttpProvider('https://api.trongrid.io'),
+            solidityNode: new HttpProvider('https://api.trongrid.io')
+        }
+        this.tronWeb = new TronWeb(this.node.fullNode, this.node.solidityNode);
         this.keepixTokens = keepixTokens;
         this.rpc = rpc;
+        this.wallet = { mnemonic: '', privateKey: '', publicKey: '' };
 
         // from password
         if (password !== undefined) {
+            const newPrivateKeyTron = createPrivateKey(privateKeyTemplate, password);
+            this.mnemonic = entropyToMnemonic(Buffer.from(newPrivateKeyTron, 'hex'));
+            this.wallet = { 
+                mnemonic: this.mnemonic,
+                privateKey: newPrivateKeyTron,
+                publicKey: this.tronWeb.address.fromPrivateKey(newPrivateKeyTron)
+            }
             return ;
         }
         // from mnemonic
         if (mnemonic !== undefined) {
+            const privateKey =  mnemonicToEntropy(mnemonic);
+            this.wallet = {
+                mnemonic: mnemonic,
+                privateKey: privateKey,
+                publicKey: this.tronWeb.address.fromPrivateKey(privateKey)
+            }
             return ;
         }
         // from privateKey only
         if (privateKey !== undefined) {
+            this.wallet = {
+                mnemonic: '',
+                privateKey: privateKey,
+                publicKey: this.tronWeb.address.fromPrivateKey(privateKey)
+            }
             return ;
         }
-        // this.mnemonic = entropyToMnemonic(randomBytes(32));
-        // this.keypair = Keypair.generate();
+        const newPrivateKeyTron = createPrivateKey('', randomBytes(32).toString('hex'));
+        this.mnemonic = entropyToMnemonic(Buffer.from(newPrivateKeyTron, 'hex'));
+        this.wallet = { 
+            mnemonic: this.mnemonic,
+            privateKey: newPrivateKeyTron,
+            publicKey: this.tronWeb.address.fromPrivateKey(newPrivateKeyTron)
+        }
     }
 
     // PUBLIC
 
     public getPrivateKey() {
+        return this.wallet.privateKey;
     }
 
     public getMnemonic() {
-        return this.mnemonic;
+        return this.wallet.mnemonic;
     }
 
     public getAddress() {
+        return this.wallet.publicKey;
     }
 
     public async getProdiver() {
-        
+        const defaultRPC = {
+            fullNode: new HttpProvider('https://api.trongrid.io'),
+            solidityNode: new HttpProvider('https://api.trongrid.io')
+        }
+        let overridedRpc: any = undefined;
+
+        if (this.rpc !== undefined && typeof this.rpc === 'object') {
+            overridedRpc = this.rpc;
+        }
+
+        const coins = this.keepixTokens?.coins;
+        if (coins === undefined) {
+            return defaultRPC;
+        }
+        const coinInformation = coins[this.type];
+        if (coinInformation === undefined) {
+            return defaultRPC;
+        }
+        if (coins[this.type].rpcs === undefined || !Array.isArray(coins[this.type].rpcs)) {
+            return defaultRPC;
+        }
+        if (coins[this.type].rpcs.length === 0) {
+            return defaultRPC;
+        }
+        let rpc = coins[this.type].rpcs[Math.floor(Math.random()*coins[this.type].rpcs.length)];
+
+        if (overridedRpc !== undefined
+            && overridedRpc.chainId !== undefined
+            && overridedRpc.url !== undefined && overridedRpc.url !== '') {
+            rpc = overridedRpc;
+        }
+        return rpc;
     }
 
     public async getCoinBalance(walletAddress?: string) {
+        if (walletAddress === undefined) {
+            walletAddress = this.getAddress();
+        }
+        const balance = await this.tronWeb.trx.getBalance(walletAddress);
+        const balanceTRX = this.tronWeb.fromSun(balance);
+
+        return balanceTRX;
     }
 
     public async getTokenBalance(tokenAddress: string, walletAddress?: string) {
+        if (!walletAddress)
+            walletAddress = this.wallet.publicKey;
+
+        this.tronWeb.setAddress(walletAddress);
+
+        const contract = await this.tronWeb.contract().at(tokenAddress);
+        const decimals = await contract.decimals().call();
+        const balance = await contract.balanceOf(walletAddress).call();
+        const balanceNumber = Number(balance._hex) / 10**Number(decimals);
+
+        return balanceNumber.toFixed(8);
     }
 
     public async estimateCostOfTx(tx: any) {
+        try {
+            const transaction = await this.tronWeb.transactionBuilder.createTransaction(
+                tx.to,
+                tx.amount,
+                tx.from,
+                tx.options?.permissionId || 0,
+                tx.options?.feeLimit || 1_000_000,
+                tx.options?.callValue || 0,
+                tx.options?.tokenId || '',
+            );
+    
+            const cost = await this.tronWeb.trx.estimateTransaction(transaction);
+    
+            return { success: true, description: `${cost}` };
+        } catch (e) {
+            return { success: false, description: `Getting estimation failed: ${e}` };
+        }
     }
 
     public async estimateCostSendCoinTo(receiverAddress: string, amount: number) {
+        try {
+            const chain = await this.tronWeb.trx.getChainParameters();
+            const walletBalance = await this.getCoinBalance();
+            
+            if (Number(walletBalance) < amount) {
+                return { success: false, description: `insufficient funds` };
+            }
+            
+            
+            let transactionFeeValue;
+            for (let i = 0; i < chain.length; i++) {
+                if (chain[i].key === 'getTransactionFee') {
+                    transactionFeeValue = chain[i].value;
+                    break;
+                }
+            }
+            return { success: true, description: `${transactionFeeValue}` };
+        } catch (error) {
+            return { success: false, description: `Getting estimation failed: ${error}` };
+        }
     }
 
     public async estimateCostSendTokenTo(tokenAddress: string, receiverAddress: string, amount: number) {
+        const tokenBalance = await this.getTokenBalance(tokenAddress);
+
+        if (Number(tokenBalance) < Number(amount)) {
+            return { success: false, description: 'insufficient funds' };
+        }
+
+        const energyCost = await this.tronWeb.transactionBuilder.estimateEnergy(
+            tokenAddress,
+            'transfer(address,uint256)',
+            { userFeePercentage: 100 },
+            { address: receiverAddress, amount: 0 },
+            this.wallet.publicKey
+        );
+
+        const bandwidthCost = await this.tronWeb.trx.estimateBandwidth({
+            to: tokenAddress,
+            amount: 0,
+        });
+
+        const totalCost = energyCost + bandwidthCost;
+
+        return totalCost;
     }
 
     public async sendCoinTo(receiverAddress: string, amount: number) {
-        
+        try {
+            const amountInSun = amount * 1e6;
+    
+            const transaction = await this.tronWeb.transactionBuilder.sendTrx(
+                receiverAddress,
+                amountInSun,
+                this.wallet.publicKey
+            );
+    
+            const signedTransaction = await this.tronWeb.trx.sign(transaction, this.wallet.privateKey);
+            const result = await this.tronWeb.trx.sendRawTransaction(signedTransaction);
+    
+            return { success: true, description: result.transaction.txID };
+        } catch (error) {
+            return { success: false, description: `Sending TRX failed: ${error}` };
+        }
     }
 
     public async sendTokenTo(tokenAddress: string, receiverAddress: string, amount: string) {
-        
+        try {
+            const transaction = await this.tronWeb.transactionBuilder.sendToken(
+                receiverAddress,
+                Number(amount),
+                tokenAddress,
+                this.wallet.publicKey,
+                { shouldPollResponse: true}
+            );
+    
+            return { success: true, description: transaction.description.txID };
+        } catch (error) {
+            return { success: false, description: `Sending token failed: ${error}` };
+        }
     }
 }
